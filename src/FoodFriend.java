c00cd258c9eb5098a8f72e64cbcbaff15d2342a3
Riley Nicholson <rnich021@odu.edu>
2026-06-17 18:58:14 -0400

    Initial commit

---
+++ src/FoodFriend.java
@@ -0,0 +1,100 @@
+import java.util.Scanner;
+import java.io.File;
+import java.io.FileReader;
+import java.io.BufferedReader;
+import java.io.FileWriter;
+import java.io.BufferedWriter;
+import java.util.ArrayList;
+import java.io.IOException;
+
+public class FoodFriend {
+	static ArrayList<String> foodItems = new ArrayList<>();
+	
+	public static void run() {
+		File saveFile = new File("SaveFile.txt");
+		
+		if (saveFile.exists()) {
+			readFile();
+		}
+		
+		menu();
+	}
+	
+	public static void readFile() {
+		try (BufferedReader reader = new BufferedReader(new FileReader("SaveFile.txt"))) {
+			String item;
+			
+			while ((item = reader.readLine()) != null) {
+				String[] temp = item.split(",");
+				
+				for (int i = 0; i < temp.length; i++) {
+					foodItems.add(temp[i]);
+				}
+			}
+		} catch (IOException e) {
+			System.out.println("Error: Save File exists, but is not found.");
+			System.exit(-1);
+		}
+	}
+	
+	public static void menu() {
+		Scanner scanner = new Scanner(System.in);
+		int input = 0;
+		
+		while (true) {
+			System.out.println("Food Friend\n-----------------");
+			System.out.println("1. Generate Recipes\n"
+					+ "2. Manage Pantry\n"
+					+ "3. Exit\n"
+					+ "\nEnter Option Here:");
+			
+			try {
+				input = scanner.nextInt();
+				
+				newPage();
+				switch (input) {
+				case 1: // Generate Recipes
+					File saveFile = new File("SaveFile.txt");
+					
+					if (saveFile.exists()) {
+						generateRecipes();
+					} else {
+						System.out.println("Error: No ingredients found in the Pantry. Recipes cannot be generated.");
+					}
+					break;
+				
+				case 2: // Manage Pantry
+					managePantry();
+					break;
+					
+				case 3: // Exit
+					break;
+					
+				default: // Error
+					System.out.println("Error: Input is not recognized. Please try again.");
+					break;
+				}
+				
+				System.out.println();
+			} catch (Exception e) {
+				newPage();
+				System.out.println("Error: Input is not valid.\n");
+				scanner.nextLine();
+			}
+		}
+	}
+	
+	public static void generateRecipes() {
+		
+	}
+	
+	public static void managePantry() {
+		
+	}
+	
+	public static void newPage() {
+		for (int i = 0; i < 15; i++) {
+			System.out.println();
+		}
+	}
+}
---
+++ src/Spoonacular.java
@@ -0,0 +1,7 @@
+
+public class Spoonacular {
+	
+	// PUT ALL OF THE API CRAP HERE
+	// OR SOMETHING LIKE THAT
+	
+}
